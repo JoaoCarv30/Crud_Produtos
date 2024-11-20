b7368408f3e914fe870579095db5a273bba7f844
Joao Victor <Carvalho.joao1@gmail.com>
2024-11-20 09:13:18 -0300

    cors and route for get image in front ok

--- src/routes.ts
+++ src/routes.ts
@@ -1,23 +1,28 @@
+import express from 'express';
 import { Router } from 'express';
 import upload from './config/multer'; // Configuração do Multer
 import { CreateProduct, GetProducts, UpdateProduct, DeleteProduct } from './controllers/productController';
 import { CreateUser, GetUsers } from './controllers/userController';
 import { Authcontroller } from './controllers/authController';
 import { AuthMiddleware } from './middlewares/authMIddleware';
-
+import path from 'path';
 
 export const router = Router();
 
-//ROTAS DE PRODUTOS
+// Servir arquivos estáticos da pasta uploads
+router.use('/uploads', express.static(path.resolve(__dirname, '../../uploads')));
+
+// ROTAS DE PRODUTOS
 router.get('/products', GetProducts);
 router.post('/products', AuthMiddleware, upload.single('Image'), CreateProduct);
-
 router.put('/products/:id', UpdateProduct);
 router.delete('/products/:id', DeleteProduct);
 
-//ROTAS DE USUÁRIOS
- router.post('/users', CreateUser);
- router.get('/users', GetUsers);
+router.use("/images", express.static("uploads")); // Rota para acessar as imagens
 
- //ROTAS DE AUTENTICACAO
- router.post('/auth', Authcontroller);
+// ROTAS DE USUÁRIOS
+router.post('/users', CreateUser);
+router.get('/users', GetUsers);
+
+// ROTAS DE AUTENTICAÇÃO
+router.post('/auth', Authcontroller);
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import { router } from './routes';
+import cors from 'cors';
+import path from 'path';
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use(router);
+
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
- 
+
